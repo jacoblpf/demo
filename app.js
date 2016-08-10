6ea0f9c785ed01c7a5bc8eab8fad8eb6f5fe7791
jacoblpf <1226187538@qq.com>
2016-08-10 10:27:17 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,206 @@
+.clear:after {
+        display: block;
+        content: "";
+        clear: both;
+      }
+      .goods-head{
+        background: #fff;
+        padding:10px 15px 0; 
+      }
+      .goods-head .goods-title {
+        font-size: 16px;
+        line-height: 30px;
+        margin:0 auto;
+      }
+      .goods-head .goods-price {
+        font-size: 20px;
+        font-weight: bold;
+        color: #e8374a;
+        line-height: 30px;
+        border-bottom: 1px solid #ccc;
+        margin: 0 -15px;
+        text-indent: 15px;
+      }
+      .goods-head .goods-state {
+        font-size: 14px;
+        line-height: 40px;
+        width: 100%;
+      }
+      .goods-head .goods-state span {
+        font-size: 14px;
+        color: #333;
+      }
+      .goods-head .goods-state .left{
+        float: left;
+        color: #8a8a8a;
+      }
+      .goods-head .goods-state .right{
+        float: right;
+        color: #8a8a8a;
+        font-size: 16px;
+        font-weight: bold;
+        color: #4e907e;
+      } 
+      .fa {
+        background: #fafafa;
+        position: relative;
+      }
+      .fix-bottom {
+        width: 100%;
+        position: fixed;
+        left: 0;
+        bottom: 0;
+      }
+      .mid-img {
+        width: 100%;
+      }  
+      .mid-img img{
+        width: 50%;
+        display: block;
+        margin: 30% auto 0;
+      }
+      .q-list {
+        margin: 0;
+        background: #f3f3f3;
+      }
+      .q-list li{
+        margin-top: 15px;
+        padding: 0 8px;
+      }
+      .q-list li:first-child{
+        margin:0;
+      }
+      .q-list li .list-title {
+        font-size: 16px;
+        margin-bottom: 5px;
+        color: #1bb394;
+      }
+      .q-list li p {
+        margin: 5px 0;
+        padding: 0 2px;
+        font-size: 14px;
+      }
+      .g-list li{
+        padding: 5px 0;
+        overflow: hidden;
+      }
+      .g-list img{
+        width: 100px;
+        height: 100px;
+        display: block;
+        float: left;
+      }
+      .g-list .g-title,
+      .g-list .g-price,
+      .g-list .g-state{
+        margin: 0;
+        line-height: 30px;
+      }
+      .g-list .g-price{
+        font-size: 16px;
+        color: #c5515c;
+      }
+      .e-list {
+
+      }
+      .e-list li {
+        margin: 10px 0;
+        padding: 5px;
+      }
+      .e-list li .e-title {
+        display: block;
+        color: #333;
+        margin: 0;
+        line-height: 30px;
+        padding: 0;
+      }
+      .e-list li .e-title .et-r {
+          position: relative;
+          max-width: 200px;
+          float:right;
+          overflow: hidden;
+          text-overflow:ellipsis;
+          white-space: nowrap;
+          padding-right: 15px;
+      }
+      .e-list li .e-title .et-r i {
+        position: absolute;
+        right: 2px;
+        margin-left: 5px;
+      }
+      .er-gray .et-r{
+        color: #bfbfbf;
+      }
+      .e-list li .e-bd img{
+        margin: 10px 0;
+      }
+      .e-list li .e-bd [class*=am-u-]{
+        padding: 0;
+        margin-top: 15px;
+      }
+      .e-list li .e-bd [class*=am-u-] span{
+        font-size: 12px;
+        display: block;
+        color: #c1c1c1;
+      }
+      .e-list li .e-bd [class*=am-u-] span em{
+        font-style: normal;
+        color: #c85761;
+      }
+      .e-list li .e-bd [class*=am-u-] button{
+        float: right;
+      }
+      .am-popup-bd .am-input-group {
+        margin-bottom: 10px;
+      }
+      .e-list-noedge {
+        margin: 0;
+      }
+      .e-list-noedge li {
+        border-top: 0;
+        margin: 0;
+      }
+      .e-list-noedge li .e-title{
+        line-height: 45px;
+      }
+      .o-tabs .am-nav-justify a{
+        padding-left:0; 
+        padding-right:0; 
+      }
+      .o-tabs .am-tabs-bd .am-tab-panel{
+        padding: 0;
+      }
+      .o-list.e-list li{
+        margin: 0 0 10px;
+      }
+      .o-list.e-list li:first-child{
+        border-top: 0;
+      }
+      .o-list.e-list li .e-title{
+        font-size: 14px;
+        border-bottom: 1px solid #ccc;
+        color: #d1d1d1;
+      }
+      .o-list.e-list li .e-title .et-r{
+       color: #f9af59;
+      }
+      .o-list.e-list li .e-bd [class*=am-u-] {
+        margin-top: 0;
+      }
+      .e-list li .e-bd {
+        position: relative;
+      }
+      .e-list li .e-bd .am-btn{
+        position: absolute;
+        right: 2px;
+        bottom: 5px;
+      }
+      .o-list.e-list li .e-bd strong {
+        font-size: 18px;
+        font-weight: normal;
+        line-height: 35px;
+      }
+      .o-list.e-list li .e-bd .o-price{
+        font-size: 18px;
+        color: #C6505E;
+      }
